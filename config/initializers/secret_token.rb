82ddba80f27a318091b6620d7e43af91cf488e30
Eric Huang <WaterFlowsEast@gmail.com>
2013-10-31 09:42:44 +0800

    delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Waterflowseast::Application.config.secret_token = '8f21348cda68b32e5fc0e7696c4d28bf02452e4769fb9b1e10a6153b9b2aa50fed65f734e0c0401520aadbc11da3efe0af1bf8fc08bd261626e024a68ae74b21'
